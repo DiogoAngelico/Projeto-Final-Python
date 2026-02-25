483a80857584c3855d4ce25b488196e0733abd9a
daleal25pi-jpg <daleal25pi@epms.pt>
2026-02-25 16:27:51 +0000

    update: novo menu

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-import json
-import menu
 
-x=input("introduz o seu nome de usuario para o word quiz:")
-print("------------------------------------------------")
-print(f"BOAS VINDAS,{x}, AO WORD QUIZ")
-print("1->  Jogar  <-")
-print("2->  regras <-")
-print("3->  sair   <-")
-print("------------------------------------------------")
-y=input("qual é a opcão que vc deseja?")
-print("------------------------------------------------")
 
-menu.jogar(y)
-menu.regras(y)
-menu.sair(y)
+def menu():
+    print("----------------------")
+    print("-----  1 Jogar   -----")
+    print("-----  2 regras  -----")
+    print("-----  3 sair    -----")    
+    print("----------------------")
+    op = input("Qual a sua escolha?")
+    return op
+
+
+def menu_1():
+   if menu_1 == "1":
+       print("Dificuldade")
+
+def menu_2():
+    if menu_2 =="2":
+        print("regras")
+
+def menu_3():
+    if menu_3 =="3":
+        print("foi um prazer ter você aqui presente no WORD QUIZ")
+
+
+while True:
+    op == menu()
+    if op == "1":
+        while True:
+            op2 = menu_1()
+        elif    
--- menu.py
+++ menu.py
@@ -1,21 +1,25 @@
 import json
 import selecionar_dificuldade
 
-def jogar(y):
-    if y == "1":
-        print("----------------------")
-        print("  dificuldades  ")
-        print("1-> dificil <-")
-        print("2->  médio  <-")
-        print("3->  fácil  <-")
-        print("----------------------")
-        
+def menu(x):
+    print("----------------------")
+    print("-----  1 Jogar   -----")
+    print("-----  2 regras  -----")
+    print("-----  3 sair    -----")    
+    print("----------------------")
+    op = input("Qual a sua escolha?")
+    return op
 
-def regras(y):
-    if y == "2":
-        print(f"Esse quiz envolve conhecimento geral, em três níveis de dificuldades, dando pontos diferentes de acordo com a dificuldade, caso seja selecionada, na dificuldade difícil irá ganhar 3 ponto por cada pergunta correta, no médio são 2 ponto por cada pergunta correta, fácil ganhará 1 ponto só, importante saber que iremos hackear as pessoas com maiores pontuações, escolher com sabedoria o nível a qual irá jogar, são 20 perguntas pra cada dificuldade" )
-                
-def sair(y):
-    if y == "3":
-        print(f"Foi um prazer ter você com a gente")
-              
+
+def menu_1(y):
+   if y == "1":
+       print()
+
+def menu_2(y):
+    if y =="2":
+        print()
+
+def menu_3(y):
+    if y =="3":
+        print("foi um prazer ter você aqui presente no Word Quiz")
+
