37506742fd06a94341e5c425001ec57a8270f946
daleal25pi-jpg <daleal25pi@epms.pt>
2026-02-25 15:23:46 +0000

    update:Inicio finalizado

--- menu.py
+++ menu.py
@@ -1,8 +1,6 @@
 import json
 import selecionar_dificuldade
 
-
-
 def jogar(y):
     if y == "1":
         print("----------------------")
@@ -10,11 +8,17 @@ def jogar(y):
         print("1-> dificil <-")
         print("2->  médio  <-")
         print("3->  fácil  <-")
+        print("----------------------")
         
 
 def regras(y):
     if y == "2":
         print(f"Esse quiz envolve conhecimento geral, em três níveis de dificuldades, dando pontos diferentes de acordo com a dificuldade, caso seja selecionada, na dificuldade difícil irá ganhar 3 ponto por cada pergunta correta, no médio são 2 ponto por cada pergunta correta, fácil ganhará 1 ponto só, importante saber que iremos hackear as pessoas com maiores pontuações, escolher com sabedoria o nível a qual irá jogar, são 20 perguntas pra cada dificuldade" )
+                
+def sair(y):
+    if y == "3":
+        print(f"Foi um prazer ter você com a gente")
+              
 '''     
 def sair(y):
     if y == "3":
