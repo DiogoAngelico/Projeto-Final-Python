4c070f022df66e5fae16195e632e3692504e0f87
DiogoAngelico <diesteves25PI@epms.pt>
2026-02-25 16:51:50 +0000

    update: menu quase feito :3

--- menu.py
+++ menu.py
@@ -2,24 +2,45 @@ import json
 import selecionar_dificuldade
 
 def menu(x):
-    print("----------------------")
-    print("-----  1 Jogar   -----")
-    print("-----  2 regras  -----")
-    print("-----  3 sair    -----")    
-    print("----------------------")
+    print("---------------------------")
+    print(f"----- BEM VINDO!!! {x} -----")
+    print("-----     1 Jogar      -----")
+    print("-----     2 regras     -----")
+    print("-----     3 sair       -----")    
+    print("---------------------------")
     op = input("Qual a sua escolha?")
     return op
 
 
-def menu_1(y):
-   if y == "1":
-       print()
+def menu_1():
+        print("---------------------------")
+        print("-----  DIFICULDADES   -----")
+        print("-----  -> Fácil       -----")
+        print("-----  -> Médio       -----")
+        print("-----  -> Díficil     -----")    
+        print("---------------------------")
 
-def menu_2(y):
-    if y =="2":
-        print()
+def menu_2():
+        print("----------------------------------")
+        print("-----     REGRAS DO JOGO     -----")
+        print("-----                        -----")
+        print("-----                        -----")    
+        print("-----                        -----")
+        print("-----                        -----")
+        print("-----                        -----")
+        print("-----                        -----")
+        print("-----                        -----")
+        print("-----                        -----")
+        print("----------------------------------")
 
-def menu_3(y):
-    if y =="3":
-        print("foi um prazer ter você aqui presente no Word Quiz")
-
+x=input("qual teu user: ")
+while True:
+    op=menu(x)
+    if op=="1":
+        op_1=menu_1()
+    elif op=="2":
+        op_2=menu_2()
+    elif op=="3":
+         print("foi um prazer ter você aqui presente no Word Quiz")
+         break
+        
