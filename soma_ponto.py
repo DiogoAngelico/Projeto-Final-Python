1c18aaf9fabffa0e151e750abbaf4cc8dfcd9312
Mateus Bitar <mabitar25pi@epms.pt>
2026-03-03 16:20:29 +0000

    Final

---
+++ soma_ponto.py
@@ -0,0 +1,14 @@
+
+#import 
+
+def dar_pontos(Dificuldade):
+    soma = 0
+    if Dificuldade == "Fácil":
+        return 1
+    elif Dificuldade == "Médio":
+        return 2
+    elif Dificuldade == "Difícil":
+        return 3
+    
+def pontos_so(a, s):
+    #\\\\
