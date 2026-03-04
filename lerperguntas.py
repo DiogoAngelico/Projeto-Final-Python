f87b761806145e4d194dcb0086149bf2e487d8e5
DiogoAngelico <diesteves25PI@epms.pt>
2026-03-04 16:50:36 +0000

    update: tinha esquecido de colocar dentro de uma função :D

--- lerperguntas.py
+++ lerperguntas.py
@@ -2,19 +2,18 @@ import json
 from pathlib import Path
 
 BASE_DIR=Path(__file__).resolve().parent
+def perguntadificil():
+    with (BASE_DIR / "dificil.json").open("r",encoding="utf-8") as f:
+        perguntas=json.load(f)
 
-with (BASE_DIR / "dificil.json").open("r",encoding="utf-8") as f:
-    perguntas=json.load(f)
 
-print(perguntas)
+def perguntafacil():
+    with (BASE_DIR / "facil.json").open("r",encoding="utf-8") as f:
+        pergunta=json.load(f)
 
-with (BASE_DIR / "facil.json").open("r",encoding="utf-8") as f:
-    pergunta=json.load(f)
 
-print(pergunta)
+def perguntamedia():
+    with (BASE_DIR / "medio.json").open("r",encoding="utf-8") as f:
+        perguntado=json.load(f)
 
-with (BASE_DIR / "medio.json").open("r",encoding="utf-8") as f:
-    perguntado=json.load(f)
-
-print(perguntado)
 
