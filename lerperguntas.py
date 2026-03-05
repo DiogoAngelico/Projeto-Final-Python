f6b09083150cf1e91b0a1d53eac030c0d1c30ae9
DiogoAngelico <diesteves25PI@epms.pt>
2026-03-05 10:10:02 +0000

    update: vou mudar de branch aq rapidinho

--- lerperguntas.py
+++ lerperguntas.py
@@ -5,15 +5,17 @@ BASE_DIR=Path(__file__).resolve().parent
 def perguntadificil():
     with (BASE_DIR / "dificil.json").open("r",encoding="utf-8") as f:
         perguntas=json.load(f)
+    return perguntas
 
 
 def perguntafacil():
     with (BASE_DIR / "facil.json").open("r",encoding="utf-8") as f:
         pergunta=json.load(f)
+    return pergunta
 
 
 def perguntamedia():
     with (BASE_DIR / "medio.json").open("r",encoding="utf-8") as f:
         perguntado=json.load(f)
-
+    return perguntado
 
--- logica.py
+++ logica.py
@@ -3,5 +3,7 @@ import lerperguntas
 from pathlib import Path
 BASE_DIR=Path(__file__).resolve().parent
 
-
+with (BASE_DIR / "dificil.json").open("r",encoding="utf-8") as f:
+    perguntas=json.load(f)
+    
 
